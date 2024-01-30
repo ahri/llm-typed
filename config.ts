6aefd37fc2f88eca122ea413aba81f303492afff
Adam Piper <adam@ahri.net>
2024-01-30 09:14:16 +0000

    Error if the OpenAI API key isn't found in the env.

--- config.ts
+++ config.ts
@@ -1,3 +1,9 @@
-export const OPENAI_API_KEY = process.env["OPENAI_API_KEY"];
+const OPENAI_API_KEY_VAR_NAME = "OPENAI_API_KEY";
+export const OPENAI_API_KEY = (() => {
+    const keyFromEnv = process.env[OPENAI_API_KEY_VAR_NAME];
+    if (!keyFromEnv) throw new Error(`Provide ${OPENAI_API_KEY_VAR_NAME} as an env var`);
+    return keyFromEnv;
+})();
+
 export const REQUEST_TIMEOUT_MS = 10_000;
 export const DEFAULT_TEMPERATURE = 0;
